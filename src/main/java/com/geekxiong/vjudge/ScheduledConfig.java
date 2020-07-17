eac087c69f008a5fe7387beeea8695efafd078bb
geekxiong <724199251@qq.com>
2020-07-17 11:11:56 +0800

    定时任务配置类

---
+++ src/main/java/com/geekxiong/vjudge/ScheduledConfig.java
@@ -0,0 +1,26 @@
+package com.geekxiong.vjudge;
+
+import com.geekxiong.vjudge.remote.RemoteDispatcher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.rmi.Remote;
+
+/**
+ * @Description Scheduled
+ * @Author xiong
+ * @Date 2020/07/17 10:30
+ * @Version 1.0
+ */
+
+@Component()
+public class ScheduledConfig {
+//    private RemoteDispatcher remoteDispatcher;
+
+//    // 每半秒钟执行,C
+//    @Scheduled(fixedRate = 500)
+//    public void judgeFetch() {
+////        remoteDispatcher;
+//        //LOGGER.info("=====>>>>>使用fixedRate{}");
+//    }
+}
