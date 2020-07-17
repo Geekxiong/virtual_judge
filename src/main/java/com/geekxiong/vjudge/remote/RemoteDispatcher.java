e4a4392179dce994481969f5518e81d6d129bf9e
geekxiong <724199251@qq.com>
2020-07-17 11:12:19 +0800

    远程调度类

---
+++ src/main/java/com/geekxiong/vjudge/remote/RemoteDispatcher.java
@@ -0,0 +1,20 @@
+package com.geekxiong.vjudge.remote;
+
+/**
+ * @Description Dispatcher
+ * @Author xiong
+ * @Date 2020/07/17 8:59
+ * @Version 1.0
+ */
+
+public class RemoteDispatcher {
+    // 首先是 爬题目
+
+    // 然后是 交题
+    public String submitCode(String oj, String probId, String code, String language){
+        return "0001";
+    }
+
+
+    // 最后是 查结果
+}
