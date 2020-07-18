430a957bede8b44d3406aacaf6dace2209313cea
geekxiong <geekxiong@foxmail.com>
2020-07-18 16:18:35 +0800

    新增通过提交Id查询提交详情

--- src/main/java/com/geekxiong/vjudge/controller/SubmissionController.java
+++ src/main/java/com/geekxiong/vjudge/controller/SubmissionController.java
@@ -2,6 +2,7 @@ package com.geekxiong.vjudge.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.geekxiong.vjudge.bean.ResponseBean;
+import com.geekxiong.vjudge.entity.Submission;
 import com.geekxiong.vjudge.service.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,4 +29,15 @@ public class SubmissionController {
         responseBean.setData(submitId);
         return responseBean;
     }
+
+    @GetMapping("/{id}")
+    public ResponseBean info(@PathVariable("id") Long submissionId){
+        ResponseBean responseBean = ResponseBean.newResponse();
+        Submission submission = submissionService.getById(submissionId);
+        responseBean.setData(submission);
+        responseBean.isSuccess();
+        return responseBean;
+    }
+
+
 }
--- src/main/java/com/geekxiong/vjudge/service/SubmissionService.java
+++ src/main/java/com/geekxiong/vjudge/service/SubmissionService.java
@@ -1,5 +1,9 @@
 package com.geekxiong.vjudge.service;
 
+import com.geekxiong.vjudge.entity.Submission;
+
 public interface SubmissionService {
     Long submitCode(Long userId, Long problemId, String code, String language);
+
+    Submission getById(Long id);
 }
